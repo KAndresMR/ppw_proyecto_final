78b808a4b2d2e1772587ba13d7835904b4577679
AndresMorocho <morochoandres12@gmail.com>
2024-05-27 11:56:26 -0500

    Configuracion Basica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'user-dashboard', component: UserDashboardComponent },
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
+  { path: 'book-details/:id', component: BookDetailsComponent },
+  { path: 'edit-book/:id', component: EditBookComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' }
+];
